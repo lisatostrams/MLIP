0a8d463406e8525bfe4c4a265426cdc19fa79e16
Lisa Tostrams <lmc.tostrams@student.ru.nl>
2019-02-26 09:24:22 +0100

    implemented decision tree

--- First_classifier.py
+++ First_classifier.py
@@ -10,3 +10,45 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 data = pd.read_csv('preprocessedTrain.csv')
+X = data.loc[:, data.columns != 'AdoptionSpeed']
+X = X.drop('Description',axis=1)
+X = X.drop('PetID',axis=1)
+X = X.drop('RescuerID',axis=1)
+
+y = data['AdoptionSpeed']
+
+attributeNames = list(X.columns.values)
+classNames = ['sameDay','firstWeek','firstMonth','2nd3rdMonth','notAdopted']
+
+#%%
+
+from sklearn import tree
+from Toolbox import treeprint
+from sklearn import model_selection
+import matplotlib.pyplot as plt
+
+test_proportion = 0.5
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X,y,test_size=test_proportion)
+levels = range(2,51)
+error = np.zeros((2,len(levels)))
+
+for t in levels:
+    dtc = tree.DecisionTreeClassifier(criterion='gini', max_depth=t)
+    dtc = dtc.fit(X_train,y_train)
+    
+    y_est_test = dtc.predict(X_test)
+    y_est_train = dtc.predict(X_train)
+    
+    test_class_error = 1-np.mean(y_est_test == y_test)
+    train_class_error = 1-np.mean(y_est_train == y_train)
+    error[0,t-2], error[1,t-2]= train_class_error, test_class_error
+
+plt.plot(levels, error[0,:])
+plt.plot(levels, error[1,:])
+plt.xlabel('Model complexity (max tree depth)')
+plt.ylabel('Error (misclassification rate)')
+plt.legend(['Error_train','Error_test']) 
+plt.title('Cross validation over tree depth')
+plt.show()  
+errorSum = sum(error)
+print('Lowest error is {:.2f} at {}'.format(min(errorSum)/2, np.argmin(errorSum)))

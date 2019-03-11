7a7017d4e8edf80ced891e415cac3432756c0ee8
Lisa Tostrams <lmc.tostrams@student.ru.nl>
2019-03-11 14:13:45 +0100

    final submission made

--- classify.py
+++ classify.py
@@ -5,9 +5,27 @@ Created on Thu Feb 28 12:49:15 2019
 @author: Lisa
 """
 #%% Attributes obtained from crossvalidation
-nAttributes = ['popularity resquer id', 'Age', 'Breed1', 'PhotoAmt', 'description length', 'sentiment', 'img_ave_contrast',
-               'State', 'Sterilized', 'img_metadata_sentiment2', 'MaturitySize', 'Color1', 'FurLength', 'Breed2',
-               'Quantity', 'Fee', 'Gender', 'cute', 'Name', 'Dewormed', 'ador', 'abandon']
+nAttributes = ['popularity resquer id',
+ 'Age',
+ 'Breed1',
+ 'PhotoAmt',
+ 'img_ave_contrast',
+ 'description length',
+ 'sentiment',
+ 'Sterilized',
+ 'Breed2',
+ 'Quantity',
+ 'State',
+ 'Fee',
+ 'vaccin',
+ 'Vaccinated',
+ 'Color1',
+ 'good',
+ 'Dewormed',
+ 'family',
+ 'mix',
+ 'vet',
+ 'ador']
 max_depth = 7
 
 n_estimators = 10
@@ -36,6 +54,10 @@ import xgboost as xgb
 from sklearn import model_selection
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.preprocessing import PowerTransformer
+from sklearn import preprocessing
+
+
 
 data = pd.read_csv('Data/preprocessedTrain2.csv') #import data
 X = data.loc[:, data.columns != 'AdoptionSpeed'] #create X without labels
@@ -50,6 +72,7 @@ y = data['AdoptionSpeed'] #label vector
 
 test = pd.read_csv('Data/preprocessedtest2.csv')
 
+
 X_test = test.drop('Description',axis=1) #drop non numerical values
 X_test = X_test.fillna(0)
 id = X_test['PetID']
@@ -59,33 +82,47 @@ X_test = X_test.drop('Unnamed: 0',axis=1)
 X_test = X_test.drop('img_metadata_label',axis=1)
 X_test = X_test[nAttributes]
 
-meta_train, meta_test, meta_y_train, meta_y_test = model_selection.train_test_split(X,y,test_size=0.5)
-Xlr_train = X
 
+non_zer0 = np.mean(X==0)==0
+zero = non_zer0[non_zer0.values==False].index
+non_zer0 = non_zer0[non_zer0.values==True].index
+
+scaler = preprocessing.PowerTransformer(method='box-cox', standardize=True).fit(X[non_zer0])
+X[non_zer0] = scaler.transform(X[non_zer0])
+X_test[non_zer0] = scaler.transform(X_test[non_zer0])
+scaler = preprocessing.StandardScaler().fit(X[zero])
+X[zero] = scaler.transform(X[zero])
+X_test[zero] = scaler.transform(X_test[zero])
+
+meta_train, meta_test, meta_y_train, meta_y_test = model_selection.train_test_split(X,y,test_size=0.1,stratify=y)
+Xlr_train = meta_train
+Xlr_m_test = meta_test
 Xlr_test = X_test
 dummy = ['State','Type','Breed1','Breed2','Gender','Color1','Color2','Color3','Vaccinated','Dewormed','Sterilized']
 for d in dummy:
     if(d in nAttributes):
-        one_hottr = pd.get_dummies(Xlr_train[d],prefix=d)
-        # Drop column d as it is now encoded
+        
+        train = pd.get_dummies(Xlr_train[d],prefix=d)
+        test = pd.get_dummies(Xlr_test[d],prefix=d)
+        m_test = pd.get_dummies(Xlr_m_test[d],prefix=d)
+        result = set(list(train))
+        result.intersection_update(list(test))
+        result.intersection_update(list(m_test))
+        one_hottr = train[list(result)]
+        one_hot = test[list(result)]
+        one_hotm = m_test[list(result)]
         Xlr_train = Xlr_train.drop(d,axis = 1)
         # Join the encoded df
         Xlr_train = Xlr_train.join(one_hottr)
         
-        one_hot = pd.get_dummies(Xlr_test[d],prefix=d)
-
-        for col in list(one_hottr):
-            if(col not in list(one_hot)):
-                one_hot[col]=0
-        for col in list(one_hot):
-            if(col not in list(one_hottr)):
-                one_hot = one_hot.drop(col,axis=1)
         Xlr_test = Xlr_test.drop(d,axis = 1)
         Xlr_test = Xlr_test.join(one_hot)
+        Xlr_m_test = Xlr_m_test.drop(d,axis=1)
+        Xlr_m_test = Xlr_m_test.join(one_hotm)
 
 
-meta_train, meta_test, meta_y_train, meta_y_test = model_selection.train_test_split(X,y,test_size=0.5)
-Xlr_train, metaXlr_test, ylr_train, ylr_test = model_selection.train_test_split(Xlr_train,y,test_size=0.5)
+
+
 #%%
 
 
@@ -106,11 +143,11 @@ predictions[:,1] = np.round(rf.predict(X_test),0)
 mlp_train[:,1] = np.round(rf.predict(meta_train),0)
 mlp_test[:,1] = np.round(rf.predict(meta_test),0)
 
-logreg = LogisticRegression(tol=tol,)
-logreg = logreg.fit(Xlr_train, ylr_train)
+logreg = LogisticRegression(tol=tol,solver='liblinear',multi_class='auto')
+logreg = logreg.fit(Xlr_train, meta_y_train)
 predictions[:,2] = logreg.predict(Xlr_test)
 mlp_train[:,2] = logreg.predict(Xlr_train)
-mlp_test[:,0] = logreg.predict(metaXlr_test)
+mlp_test[:,2] = logreg.predict(Xlr_m_test)
 
 knn = KNeighborsClassifier(nn)
 knn = knn.fit(meta_train, meta_y_train)
@@ -118,7 +155,7 @@ predictions[:,3] = knn.predict(X_test)
 mlp_train[:,3] = knn.predict(meta_train)
 mlp_test[:,3] = knn.predict(meta_test)
 
-svm = SVC(tol=tol)
+svm = SVC(tol=tol,gamma='auto')
 svm = svm.fit(meta_train, meta_y_train)
 predictions[:,4] = svm.predict(X_test)
 mlp_train[:,4] = svm.predict(meta_train)
@@ -135,10 +172,8 @@ mlp_test[:,5] = knn.predict(meta_test)
 d_train = xgb.DMatrix(data=meta_train, label=meta_y_train, feature_names=meta_train.columns)
 d_val = xgb.DMatrix(data=meta_test,label=meta_y_test, feature_names=meta_test.columns)
 evallist = [(d_val, 'eval'), (d_train, 'train')]
-model = xgb.train(dtrain=d_train, num_boost_round=30000, evals=evallist, early_stopping_rounds=3000,  params=xgb_params)
+model = xgb.train(dtrain=d_train, num_boost_round=30000, evals=evallist, early_stopping_rounds=3000, verbose_eval=3000, params=xgb_params)
 predictions[:,6] = np.round(model.predict(xgb.DMatrix(X_test, feature_names=X_test.columns), ntree_limit=model.best_ntree_limit),0)
-
-
 mlp_train[:,6] = np.round(model.predict(xgb.DMatrix(meta_train, feature_names=meta_train.columns), ntree_limit=model.best_ntree_limit),0)
 mlp_test[:,6] = np.round(model.predict(xgb.DMatrix(meta_test, feature_names=meta_test.columns), ntree_limit=model.best_ntree_limit),0)
 
@@ -151,6 +186,7 @@ for i in range(len(classifiers)):
     print('Train error for {} is: {:.4f}'.format(classifiers[i],err))
     err = 1-np.mean(mlp_test[:,i]==meta_y_test)
     print('Test error for {} is: {:.4f}'.format(classifiers[i],err))
+    print()
     correct[:,i] = mlp_train[:,i] == meta_y_train
     correct_test[:,i] = mlp_test[:,i] == meta_y_test
     if(min(mlp_train[:,i])<0):
@@ -171,29 +207,56 @@ print('In total, {:.2f}% of the meta training set is classified correctly by at 
 print('In total, {:.2f}% of the meta test set is classified correctly by at least one classifier'.format(correct_testdf.max(axis=1).mean()*100))
 
 #%%
+def hist(y):
+    histy = np.zeros((5,))
+    for i in range(0,5):
+        histy[i] = np.mean(y==i)
+    return histy
+
+def sse_hist(h1,h2,weight=[1,1,1,1,1]):
+    sse = 0
+    for i in range(0,5):
+        sse = sse + weight[i]*(h1[i] - h2[i])**2
+    return sse
+#%%
+histy = hist(y)     
 
 from sklearn.neural_network import MLPClassifier
 models = []
-errors= []
-for i in range(1,15):
+accuracy = []
+models_sse = []
+sse = []
+for i in range(1,10):
     model_j = []
-    error_j = []
+    score_j = []
+    sse_j = []
     for j in range(0,10):
         clf = MLPClassifier(solver='lbfgs', hidden_layer_sizes=(i,))
-        clf.fit(mlp_train, meta_y_train)
+        clf.fit(mlp_test, meta_y_test)
         model_j.append(clf)
-        error_j.append(clf.score(mlp_test,meta_y_test))
+        score_j.append(clf.score(mlp_test,meta_y_test))
+        hist_est = hist(clf.predict(predictions))
+        weights = 1-histy**2
+        sse_j.append(sse_hist(hist_est,histy,weight=weights))
+        
     
-    print("Layer {}".format(i))
-
-    print(max(error_j))
+    print("Layer {} test accuracy: {:.4f}".format(i,max(score_j)))
+    print("Layer {} lowest SSE hist: {:.4f}".format(i,min(sse_j)))
+    print()
     
-    models.append(model_j[np.argmax(error_j)-1])
-    errors.append(max(error_j))
+    models.append(model_j[np.argmax(score_j)])
+    accuracy.append(max(score_j))
+    sse.append(sse_j[np.argmax(score_j)])
+    
     
 #%%
-model = np.argmax(error_j)
-print('Best number of hlayers = {}'.format(model)) 
+model_acc = np.argmax(accuracy)
+print('Best number of hlayers test acc = {}'.format(model_acc+1)) 
+model_sse = np.argmin(sse)
+print('Best number of hlayers hist sse = {}'.format(model_sse+1)) 
+s = [accuracy[i]-sse[i] for i in range(0,7)]
+model = np.argmax(s)
+print('Best number of hlayers total = {}'.format(model+1)) 
 #%%
 clf = models[model]
 print(clf.score(mlp_train,meta_y_train)) 
@@ -206,4 +269,10 @@ y_est = clf.predict(predictions)
 submission = pd.DataFrame(index=X_test.index)
 submission['PetID'] = id
 submission['AdoptionSpeed'] = y_est
+submission['AdoptionSpeed'] = submission['AdoptionSpeed'].astype(int)
 submission.to_csv('submission.csv',index=False)
+
+histy = y.hist()
+submission.hist()
+plt.show()
+
--- dtc.py
+++ dtc.py
@@ -22,45 +22,44 @@ X = X.drop('Unnamed: 0',axis=1)
 X = X.drop('img_metadata_label',axis=1)
 y = data['AdoptionSpeed'] #label vector
 
-test_proportion = 0.5  # set crossval proportion
+test_proportion = 0.25  # set crossval proportion
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X,y,test_size=test_proportion)
 
 
-#%%
-
-levels = range(2,51)
-error = np.zeros((2,len(levels)))
-
-for t in levels:
-    dtc = tree.DecisionTreeClassifier(criterion='gini', max_depth=t) #train decision tree
-    dtc = dtc.fit(X_train,y_train)
-    
-    y_est_test = dtc.predict(X_test)
-    y_est_train = dtc.predict(X_train)
-    
-    test_class_error = 1-np.mean(y_est_test == y_test)
-    train_class_error = 1-np.mean(y_est_train == y_train)
-    error[0,t-2], error[1,t-2]= train_class_error, test_class_error
-
-plt.plot(levels, error[0,:])
-plt.plot(levels, error[1,:])
-plt.xlabel('Model complexity (max tree depth)')
-plt.ylabel('Error (misclassification rate)')
-plt.legend(['Error_train','Error_test']) 
-plt.title('Cross validation over tree depth')
-plt.show()  
-
-
-print('Lowest test error is {:.4f} at {}'.format(min(error[1,:]), levels[np.argmin(error[1,:])]))
-
-minl = levels[np.argmin(error[1,:])]
+##%%
+#
+#levels = range(2,51)
+#error = np.zeros((2,len(levels)))
+#
+#for t in levels:
+#    dtc = tree.DecisionTreeClassifier(criterion='gini', max_depth=t) #train decision tree
+#    dtc = dtc.fit(X_train,y_train)
+#    
+#    y_est_test = dtc.predict(X_test)
+#    y_est_train = dtc.predict(X_train)
+#    
+#    test_class_error = 1-np.mean(y_est_test == y_test)
+#    train_class_error = 1-np.mean(y_est_train == y_train)
+#    error[0,t-2], error[1,t-2]= train_class_error, test_class_error
+#
+#plt.plot(levels, error[0,:])
+#plt.plot(levels, error[1,:])
+#plt.xlabel('Model complexity (max tree depth)')
+#plt.ylabel('Error (misclassification rate)')
+#plt.legend(['Error_train','Error_test']) 
+#plt.title('Cross validation over tree depth')
+#plt.show()  
+#
+#
+#print('Lowest test error is {:.4f} at {}'.format(min(error[1,:]), levels[np.argmin(error[1,:])]))
+#
+#minl = levels[np.argmin(error[1,:])]
 dtc = tree.DecisionTreeClassifier(criterion='gini',max_depth=minl) #train decision tree
 dtc = dtc.fit(X_train,y_train)
 
 
 
 
-#%%
 attributeNames = list(X)
 levels = range(1,len(attributeNames))
 error = np.zeros((2,len(levels)))
